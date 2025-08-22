d315e9c9e0b453578355c174e209555b451188c2
cockburncolin <51009168+cockburncolin@users.noreply.github.com>
2025-08-21 19:50:49 -0700

    Clean things up

--- src/dice.rs
+++ src/dice.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::iter::repeat;
 use std::vec::Vec;
 
 pub type DieStack = Vec<Die>;
@@ -15,8 +16,42 @@ impl Die {
         Die {
             name: String::from(""),
             sides: 0,
-            result: 0
+            result: 0,
         }
+    }
+}
+
+pub trait Processable {
+    fn from_vec(input: Vec<String>) -> Self;
+}
+
+impl Processable for DieStack {
+    /// Process the strings provided by the user
+    fn from_vec(input: Vec<String>) -> Self {
+        let mut ret_stack = DieStack::new();
+
+        for str in input {
+            let mut die = Die::new();
+            let upper_string = str.to_uppercase();
+            let dice_string: Vec<&str> = upper_string.split("D").collect();
+
+            let count: usize = if dice_string[0].is_empty() {
+                1
+            } else {
+                dice_string[0]
+                    .parse()
+                    .expect("Count provided wasn't a number")
+            };
+
+            let sides: u32 = dice_string[1].parse().expect("Sides provided not a number");
+            let name = format!("D{sides}");
+
+            die.sides = sides;
+            die.name = name;
+
+            ret_stack.extend(repeat(die).take(count)); // Push die onto stack count times
+        }
+        return ret_stack;
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 mod dice;
 
-use std::iter::repeat;
-
 use clap::Parser;
-use dice::{Die, DieStack, Outputable, Rollable};
+use dice::{DieStack, Outputable, Rollable, Processable};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -14,39 +12,9 @@ struct Args {
     dice_str: Vec<String>,
 }
 
-/// Process the strings provided by the user
-fn process_string(input: Vec<String>) -> DieStack {
-    let mut ret_stack = DieStack::new();
-
-    for str in input {
-        let mut die = Die::new();
-        let upper_string = str.to_uppercase();
-        let dice_string: Vec<&str> = upper_string.split("D").collect();
-
-        let count: usize = if dice_string[0].is_empty() {
-            1
-        } else {
-            dice_string[0]
-                .parse()
-                .expect("Count provided wasn't a number")
-        };
-
-        let sides: u32 = dice_string[1].parse().expect("Sides provided not a number");
-        let name = format!("D{sides}");
-
-        die.sides = sides;
-        die.name = name;
-
-        ret_stack.extend(repeat(die).take(count)); // Push die onto stack count times
-    }
-    return ret_stack;
-}
-
 fn main() {
     let args: Args = Args::parse();
-    let mut stack = DieStack::new();
-
-    stack.extend(process_string(args.dice_str));
+    let mut stack = DieStack::from_vec(args.dice_str);
 
     stack.roll();
     stack.print_results();
